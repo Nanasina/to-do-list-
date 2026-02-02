523ad7197c357bc59b086655d603f01ba8c2bd6e
Nanasina <hasinabefanambinana09@gmail.com>
2026-02-02 21:52:39 +0100

    Bouton ajouter fait

--- to_do_list/src/App.jsx
+++ to_do_list/src/App.jsx
@@ -1,12 +1,10 @@
 import Saisie from "./Saisie";
-import List from "./List";
 
 function App() {
 
   return (
     <>
       <Saisie />
-      <List />
     </>
   )
 }
--- to_do_list/src/Saisie.jsx
+++ to_do_list/src/Saisie.jsx
@@ -1,18 +1,48 @@
 import {CirclePlus} from "lucide-react";
+import { useState } from "react";
+
 
 function Saisie() {
+  const [tache, setTache] = useState("")
+  const [taches, setTaches] = useState([])
+
+  const handelchange = (e) =>{
+    setTache(e.target.value)
+  }
+
+  const addTache = () =>{
+    if(tache === ""){
+      return;
+    }
+    setTaches([...taches, tache])
+    setTache("")
+  }
 
   return (
     <>
       <div className="m-20 p-0 flex gap-2">
-        <input className="border rounded-xl p-1.5 w-70" type="text" placeholder="Saisir une tâche..."/>
+        <input 
+        className="border rounded-xl p-1.5 w-70" 
+        type="text"
+        value={tache}
+        onChange={handelchange}
+        placeholder="Saisir une tâche..."
+        />
         <div className="w-32 flex-initial">
-            <button className="btn btn-soft btn-primary rounded-lg ">
+            <button 
+            className="btn btn-soft btn-primary rounded-lg "
+            onClick={addTache}
+            >
                 <CirclePlus />
                 Ajouter
                 </button>
         </div>
       </div>
+      <div className="bg-primary/10 w-180 ml-10 rounded-xl">
+        {taches.map((t, index) => (
+          <p key={index} className="p-3">{t}</p>
+        ))} 
+        </div>
     </>
   )
 }

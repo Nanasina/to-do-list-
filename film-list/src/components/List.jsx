e535bc2da187b354bad6a01040e1a5bade725851
Nanasina <hasinabefanambinana09@gmail.com>
2026-02-13 22:08:05 +0100

    Ajout d'un modal pour le bouton ajouter

--- film-list/src/components/List.jsx
+++ film-list/src/components/List.jsx
@@ -1,10 +1,14 @@
+import { Plus, Clapperboard } from "lucide-react";
+// import { motion, Typewriter } from "motion/react"
+
 function List() {
 
   return (
     <>
       <div className="flex gap-8 m-8">
-        <h1 className="font-mono text-3xl font-bolds">FILMS & SERIES</h1>
-        <label className="input">
+        <h1 className="font-mono text-3xl font-bolds flex"> <Clapperboard className="m-2 animate-bounce"/>FILMS & SERIES</h1>
+        <div>
+          <label className="input">
             <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <g
                   strokeLinejoin="round"
@@ -19,24 +23,27 @@ function List() {
          </svg>
            <input type="search" placeholder="Recherche" />
      </label>
+        </div>
+        
      <button className="btn btn-soft">Tous</button>
      <button className="btn btn-soft">A regarder</button>
      <button className="btn btn-soft">Vu</button>
 
      <div className="ml-15">
-        <button className="btn btn-soft btn-secondary">Ajouter un film/série</button>
+        <button className="btn btn-soft btn-secondary " onClick={()=>document.getElementById('my_modal_3').showModal()}> <Plus />Ajouter un film/série</button>
      </div>
      
     </div>
 
-    <div className="m-8 ">
-        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+    <div className="m-8 rounded-lg border ">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 ">
   <table className="table">
     {/* head */}
     <thead>
       <tr>
         <th>Titre</th>
-        <th>Type</th>
+        <th>Film ou Série</th>
+        <th>Catégorie</th>
         <th>Status</th>
         <th>Nombre de vu</th>
         <th>Actions</th>
@@ -46,11 +53,49 @@ function List() {
     <tbody>
       {/* row 1 */}
       <tr>
-        
+        <td>hsgsfs</td>
       </tr>
     </tbody>
   </table>
 </div>
+    </div>
+
+    <div>
+      <dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-lg text-center">AJOUTER UN FILM OU UNE SERIE</h3>
+   <div className="m-8">
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Titre</legend>
+      <input type="text" className="input w-95" placeholder="Titre du film" />
+  </fieldset>
+
+   <fieldset className="fieldset">
+      <legend className="fieldset-legend">Film ou série</legend>
+      <select defaultValue="Film ou série" className="select w-95">
+        <option disabled={true} >Film ou série</option>
+        <option>Film</option>
+        <option>Série</option>
+        
+     </select>
+  </fieldset>
+
+   <fieldset className="fieldset">
+      <legend className="fieldset-legend">Catégorie</legend>
+      <input type="text" className="input w-95" placeholder="exemple: K-drama" />
+  </fieldset>
+   </div>
+
+   <div className="flex justify-center items-center">
+    <button className="btn btn-soft btn-secondary btn-wide ">AJOUTER</button>
+   </div>
+    
+  </div>
+</dialog>
     </div>
     </>
   )
